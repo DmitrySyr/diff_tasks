85d325c60c9827cc463cc47fb51da211e732ef97
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-13 15:50:18 +0000

    Deploy DmitrySyr/diff_tasks to github.com/DmitrySyr/diff_tasks.git:gh-pages

--- otus_task_4/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/travis/build/DmitrySyr/diff_tasks/otus_task_4
-# Build directory: /home/travis/build/DmitrySyr/diff_tasks/otus_task_4
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
